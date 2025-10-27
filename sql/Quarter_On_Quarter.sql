421473d7072b44c8df3bcc060fe9b7dc528478bd
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-10-27 06:45:16 -0700

    Update Quarter_On_Quarter.sql

--- sql/Quarter_On_Quarter.sql
+++ sql/Quarter_On_Quarter.sql
@@ -1,13 +1,53 @@
 WITH qoq AS (
 	SELECT 
 		quarterly,
+	
         Online_marketing_services_and_others,
+	
 		((Online_marketing_services_and_others - 
 			LAG(Online_marketing_services_and_others,1) 
 				OVER (ORDER BY quarterly ASC))
 			/LAG(Online_marketing_services_and_others,1)
-				OVER (ORDER BY quarterly ASC) * 100) AS qoqr
-		FROM temu
+				OVER (ORDER BY quarterly ASC) * 100) 
+					AS qoqr
+	FROM temu
+),
+
+revenue AS (
+	SELECT
+	
+		quarterly,
+	    
+		Online_marketing_services_and_others + 
+			transaction_services 
+				AS revenue,
+	        
+	    Online_marketing_services_and_others + 
+			transaction_services - costs_of_revenues 
+				AS gross_margin,
+		
+		(Online_marketing_services_and_others + 
+			transaction_services)
+				/sales_and_marketing_expenses 
+					AS marketing_efficiency_ratio,
+	                
+		operating_profit / 
+			(Online_marketing_services_and_others + 
+				transaction_services)
+					AS operating_margin,
+                
+		Receivables_from_online_payment_platforms, -- plot this against revenue
+		
+	    Cash_and_cash_equivalents, -- plot this vs revenue
+	    
+	    short_term_investments, -- plot this vs. quarters
+	    
+	    Net_cash_generated_from_operating_activities -- plot this vs. quarters
+	    
+	    
+    FROM temu
 )
 
-SELECT * FROM qoq;
+-- SELECT * FROM qoq;
+
+-- SELECT * FROM temu
