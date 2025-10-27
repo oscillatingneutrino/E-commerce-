db2e83bae4119afc698ae991c586660116acf118
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-10-27 02:42:36 -0700

    Create create_table.sql

---
+++ sql/create_table.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE temu (
+Quarterly TEXT,
+Online_marketing_services_and_others INT,
+Transaction_Services INT,
+sales_and_marketing_expenses INT,
+operating_profit INT,
+Receivables_from_online_payment_platforms INT,
+Prepayments_and_other_current_assets INT,
+Cash_and_cash_equivalents INT,
+short_term_investments INT,
+Net_cash_generated_from_operating_activities INT
+);
