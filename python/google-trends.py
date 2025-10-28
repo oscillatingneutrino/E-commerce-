b2d5d1fb8e870131f8193b5a926655af765e610b
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-10-28 05:02:03 -0700

    Update google-trends.py

--- python/google-trends.py
+++ python/google-trends.py
@@ -11,28 +11,34 @@ x = 'Week'
 
 y = 'temu'
 
-
+df_75['Month'] = pd.to_datetime(df_75[x], unit='D', origin = '1899-12-30')
 
 
 plt.figure(figsize=(10,5), dpi = 200)
 
-ax = df_75.plot(x, y , ax=plt.gca(), label ='Search Count')
-
+plt.plot(df_75['Month'], df_75[y],label ='Search Count', linewidth=2.5)
+#
 z = np.polyfit(df_75[x], df_75[y] , 1)
 
 p = np.poly1d(z)
 
-plb.plot(df_75[x],p(df_75[x]), color = 'purple', linestyle = '-.', label = 'Trendline')
+plb.plot(df_75['Month'],p(df_75[x]), color = 'purple', linestyle = '-.', label = 'Trendline', linewidth=2.5)
 
 
-for i,x_val in enumerate([45689, 45749, 45789, 45807, 45819, 45835]):
+dates = pd.to_datetime([45689, 45749, 45789, 45807, 45819, 45835], unit='D', origin = '1899-12-30')
+
+for i,x_val in enumerate(dates):
     if i == 0:
         plt.axvline(x=x_val, color = 'red', linestyle = '--', label = 'Tariff Announcement')
     else:
         plt.axvline(x=x_val, color = 'red', linestyle = '--')
 
-plt.title('Temu Searches Through the Course of Tariffs')
+plt.title('Temu Searches Through the Course of Tariffs', fontsize = 14)
 
-plt.xlabel('Date Number')
+plt.grid(True, 'major')
+
+plt.xlabel('Dates')
 plt.ylabel('Search Count')
-plt.legend(loc='center', bbox_to_anchor=(0.25, 0.2))
+plt.legend(loc='center', bbox_to_anchor=(0.25, 0.25))
+
+plt.show()
